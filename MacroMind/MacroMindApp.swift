1115f5d298e32d7a7b79d6769eba133ada9dd729
Danilo Lekovic <danilo_lekovic@sfu.ca>
2023-12-07 22:08:53 -0800

    Initial Commit

---
+++ MacroMind/MacroMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroMindApp.swift
+//  MacroMind
+//
+//  Created by Danilo Lekovic on 2023-12-07.
+//
+
+import SwiftUI
+
+@main
+struct MacroMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
